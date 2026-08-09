32cf77718dfe85d4d8effde48d9dbb572f7bb93f
Suraj Mane <surajsmane11@gmail.com>
2026-08-09 12:39:55 +0530

    Initial Django project

---
+++ store/views.py
@@ -0,0 +1,36 @@
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from .forms import RegisterForm
+from django.contrib import messages
+
+def home(request):
+    return render(request, 'home.html')
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful.")
+            return redirect('home')
+    else:
+        form = RegisterForm()
+    return render(request, 'register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "Invalid username or password.")
+    return render(request, 'login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
