a88f6be3509eeee031d4605d1eb7e6c5fcc9c816
Suraj Mane <surajsmane11@gmail.com>
2026-08-16 13:29:43 +0530

    fix jenkin file  issue 3

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -30,8 +30,6 @@ ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
 ]
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -128,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
